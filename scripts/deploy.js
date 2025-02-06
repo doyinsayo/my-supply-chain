b9bf6c5901ffd9dacced5f086f1588a300c926c8
doyinsayo <doyinsa01@gmail.com>
2025-02-06 13:28:33 +0100

    deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,21 @@
 async function main() {
+    // Get the contract factory for the SupplyChain contract
     const SupplyChain = await ethers.getContractFactory("SupplyChain");
+    
+    // Deploy the contract
     const supplyChain = await SupplyChain.deploy();
-    await supplyChain.deployed();
-    console.log("SupplyChain deployed to:", supplyChain.address);
+    
+    // Wait for the deployment to be mined
+    await supplyChain.waitForDeployment();
+
+    // Log the contract address to the console
+    console.log("SupplyChain deployed to:",await supplyChain.getAddress());
 }
 
+// Execute the main function
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
+    .then(() => process.exit(0)) // Exit the process if successful
+    .catch((error) => { // Handle errors
         console.error(error);
         process.exit(1);
     });
